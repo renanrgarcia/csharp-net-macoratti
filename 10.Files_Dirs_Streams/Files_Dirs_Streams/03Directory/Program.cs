8457ce27325c66c1f66c8248b287a48fcec344c7
Renan Garcia <renanrgarcia@gmail.com>
2025-06-12 11:04:36 -0300

    Add project files and implementations for directory and directory info operations, including creation, deletion, and listing of directories and files, along with related documentation.

---
+++ 10.Files_Dirs_Streams/Files_Dirs_Streams/03Directory/Program.cs
@@ -0,0 +1,103 @@
+﻿var directoryPath = @"D:\Data\MyDirectory";
+
+Console.WriteLine("\n Creating a new directory:");
+
+try
+{
+    if (!Directory.Exists(directoryPath))
+    {
+        Directory.CreateDirectory(directoryPath);
+        Console.WriteLine($"Directory created at: {directoryPath}");
+    }
+    else
+    {
+        Console.WriteLine($"Directory already exists at: {directoryPath}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while creating the directory: {ex.Message}");
+}
+
+Console.WriteLine("\n Excluding a directory:");
+
+try
+{
+    if (Directory.Exists(directoryPath))
+    {
+        Directory.Delete(directoryPath);
+        Console.WriteLine($"Excluded directory at: {directoryPath}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while excluding the directory: {ex.Message}");
+}
+
+Console.WriteLine("\n Listing subdirectories of a directory:");
+var baseDirectoryPath = @"D:/Data";
+
+try
+{
+    if (Directory.Exists(baseDirectoryPath))
+    {
+        string[] subDirectories = Directory.GetDirectories(baseDirectoryPath, "*t");
+        foreach (var subDir in subDirectories)
+        {
+            Console.WriteLine($"Subdirectory: {subDir}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The specified directory does not exist.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while listing directories: {ex.Message}");
+}
+
+Console.WriteLine("\n Getting the files of a directory:");
+
+try
+{
+    if (Directory.Exists(baseDirectoryPath))
+    {
+        string[] files = Directory.GetFiles(baseDirectoryPath, "*.txt");
+        foreach (var file in files)
+        {
+            Console.WriteLine($"File: {file}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The specified directory does not exist.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while getting files: {ex.Message}");
+}
+
+Console.WriteLine("Renaming (Moving) a directory:");
+
+var sourceDirectoryPath = @"D:\Data\txt";
+
+var destinationDirectoryPath = @"D:\Data\new_txt";
+
+try
+{
+    if (Directory.Exists(sourceDirectoryPath))
+    {
+        Directory.Move(sourceDirectoryPath, destinationDirectoryPath);
+        Console.WriteLine($"Directory moved from {sourceDirectoryPath} to {destinationDirectoryPath}");
+    }
+    else
+    {
+        Console.WriteLine("The source directory does not exist. Cannot move.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while moving the directory: {ex.Message}");
+}
---
+++ 10.Files_Dirs_Streams/Files_Dirs_Streams/04DirectoryInfo/Program.cs
@@ -0,0 +1,92 @@
+﻿var directoryPath = @"D:\Data";
+
+var originDirectory = new DirectoryInfo(directoryPath);
+
+Console.WriteLine("Directory name: " + originDirectory.Name);
+Console.WriteLine("Directory full path: " + originDirectory.FullName);
+Console.WriteLine("Directory creation time: " + originDirectory.CreationTime);
+Console.WriteLine("Directory last write time: " + originDirectory.LastWriteTime);
+Console.WriteLine("Directory attributes: " + originDirectory.Attributes);
+
+Console.WriteLine("Creating a new directory:");
+var newDirectory = new DirectoryInfo(@"D:\Data\NewDir");
+try
+{
+    if (!newDirectory.Exists)
+    {
+        newDirectory.Create();
+        Console.WriteLine($"Directory created at: {newDirectory.FullName}");
+    }
+    else
+    {
+        Console.WriteLine($"Directory already exists at: {newDirectory.FullName}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while creating the directory: {ex.Message}");
+}
+
+Console.WriteLine("Deleting a directory:");
+try
+{
+    if (newDirectory.Exists)
+    {
+        newDirectory.Delete();
+        Console.WriteLine($"Directory deleted at: {newDirectory.FullName}");
+    }
+    else
+    {
+        Console.WriteLine($"Directory does not exist at: {newDirectory.FullName}");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while deleting the directory: {ex.Message}");
+}
+
+Console.WriteLine("Exhibiting subdirectories:");
+
+var direcoryBase = @"D:";
+
+try
+{
+    if (Directory.Exists(direcoryBase))
+    {
+        var subDirectories = Directory.GetDirectories(direcoryBase, "*t");
+        foreach (var subDir in subDirectories)
+        {
+            Console.WriteLine($"Subdirectory: {subDir}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The specified directory does not exist.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while listing directories: {ex.Message}");
+}
+
+Console.WriteLine("Get files:");
+
+try
+{
+    if (originDirectory.Exists)
+    {
+        var files = originDirectory.GetFiles();
+        foreach (var file in files)
+        {
+            Console.WriteLine($"File: {file.Name}, Size: {file.Length} bytes, Last Modified: {file.LastWriteTime}");
+        }
+    }
+    else
+    {
+        Console.WriteLine("The specified directory does not exist.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred while getting files: {ex.Message}");
+}
