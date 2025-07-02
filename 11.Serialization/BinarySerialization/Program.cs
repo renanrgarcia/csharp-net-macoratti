862e2daa0356d100d5e0923a06f92667e3350190
Renan Garcia <renanrgarcia@gmail.com>
2025-07-02 11:04:43 -0300

    Add project files and implementations for binary serialization, including Student class and serialization logic, along with related documentation updates.

---
+++ 11.Serialization/BinarySerialization/Program.cs
@@ -0,0 +1,22 @@
+﻿using BinarySerialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+Student student = new Student(1, "John Doe", 20);
+
+var pathFile = @"C:\\temp\\student.bin";
+
+using (FileStream fileStream = new FileStream(pathFile, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+{
+#pragma warning disable SYSLIB0011 // BinaryFormatter is obsolete
+    BinaryFormatter formatter = new BinaryFormatter();
+    formatter.Serialize(fileStream, student); // Error: SYSLIB0011: BinaryFormatter is obsolete
+#pragma warning disable SYSLIB0011
+
+    Console.WriteLine("Type anything to Deserialize the object...");
+    Console.ReadKey();
+    fileStream.Seek(0, SeekOrigin.Begin);
+    var deserializedStudent = (Student)formatter.Deserialize(fileStream);
+    Console.WriteLine(deserializedStudent.Name);
+}
+
+Console.WriteLine("Student object serialized successfully.");
---
+++ 11.Serialization/BinarySerialization/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace BinarySerialization;
+[Serializable]
+internal class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+
+    [NonSerialized]
+    public int Age;
+
+    public Student(int id, string name, int age)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+    }
+}
