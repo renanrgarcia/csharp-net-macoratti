fbbbdd68fbacce0c21fd1d206ba0157abad17e87
Renan Garcia <renanrgarcia@gmail.com>
2025-07-17 08:14:25 -0300

    Add initial project files for synchronous programming example, including solution, project, and main program files.

---
+++ 12.AsynchronousProgramming/SyncProgramming/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("### Synchronous Programming Example ###");
+
+int wait = 4000; // milliseconds or 4 seconds
+
+Console.WriteLine("Type anything to start");
+Console.ReadLine();
+
+CallTask(wait);
+
+Console.WriteLine("Time elapsed: {0} seconds", wait / 1000);
+Console.WriteLine("End of synchronous programming example");
+
+Console.ReadKey();
+
+static void CallTask(int wait)
+{
+    Console.WriteLine("Starting task...");
+    //Task.Delay(TimeSpan.FromSeconds(wait));
+    Thread.Sleep(wait);
+    Console.WriteLine("Task completed.");
+}
