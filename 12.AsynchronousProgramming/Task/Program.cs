78298357c87b336c551c8fe1a9fbcc6e1bc0849c
Renan Garcia <renanrgarcia@gmail.com>
2025-07-18 09:46:48 -0300

    Add projects and implementations for asynchronous programming, including Task and ValueTask examples, cancellation handling, and related documentation updates.

---
+++ 12.AsynchronousProgramming/Task/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Testing Task method...");
+await DoSomethingAsync();
+async Task DoSomethingAsync()
+{
+    // Simulate some asynchronous work
+    await Task.Delay(1000);
+    Console.WriteLine("Done!");
+}
+
+Console.WriteLine("Testing Task<T> method...");
+
+var result = await GetNumberAsync();
+Console.WriteLine($"The result is: {result}");
+
+async Task<int> GetNumberAsync()
+{
+    // Simulate some asynchronous work
+    await Task.Delay(1000);
+    return 42; // Returning an integer value
+}
---
+++ 12.AsynchronousProgramming/ValueTaskT/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Type the first number:");
+int firstNumber = int.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine("Type the second number:");
+int secondNumber = int.Parse(Console.ReadLine() ?? "0");
+
+var sum = await AddNumbers(firstNumber, secondNumber); // Await the ValueTask directly
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"The sum of {firstNumber} and {secondNumber} is: {sum}");
+
+Console.ReadKey();
+
+static async ValueTask<int> AddNumbers(int a, int b)
+{
+    if (a == 0 && b == 0)
+        return 0;
+
+    return await Task.Run(() => a + b); // Task.Run is used to offload the computation to a thread pool thread  
+}
+
+//static async Task<int> AddNumbersTask(int a, int b)
+//{
+//    if (a == 0 && b == 0)
+//        return 0;
+
+//    return await Task.Run(() => a + b);
+//}
